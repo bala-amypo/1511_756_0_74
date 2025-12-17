5c2562ed6f96a8829089fe0e3a9ad4df59570b8b
bala-amypo <bala@amypo.in>
2025-12-17 10:05:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoctionEntity.java
+++ src/main/java/com/example/demo/entity/LoctionEntity.java
@@ -1,25 +1,22 @@
-
 package com.example.demo.entity;
 
-
 import jakarta.persistence.Entity;
-
-
-
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-
-public class Location {
+public class LocationEntity {
     @Id
-    private int id;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
     private String name;
-    private double latitude;
-    private double longitude;
-    public int getId() {
+    private long latitude;
+    private long longitude;
+    public long getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
     public String getName() {
@@ -28,25 +25,25 @@ public class Location {
     public void setName(String name) {
         this.name = name;
     }
-    public double getLatitude() {
+    public long getLatitude() {
         return latitude;
     }
-    public void setLatitude(double latitude) {
+    public void setLatitude(long latitude) {
         this.latitude = latitude;
     }
-    public double getLongitude() {
+    public long getLongitude() {
         return longitude;
     }
-    public void setLongitude(double longitude) {
+    public void setLongitude(long longitude) {
         this.longitude = longitude;
     }
-    public Location(int id, String name, double latitude, double longitude) {
+    public LocationEntity() {
+    }
+    public LocationEntity(long id, String name, long latitude, long longitude) {
         this.id = id;
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
     }
-    public Location() {
-    }
-
-}  
+    
+}
--- src/main/java/com/example/demo/repository/LocationR.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.repository;    
-import com.example.demo.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;   
-public interface LocationR extends JpaRepository<Location, Long> {  
-}
